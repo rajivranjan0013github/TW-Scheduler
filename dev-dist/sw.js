5d4a1224619bf8018087ce9bc855a8649762e5da
ayushk9799 <ayushkumarsanu00@gmail.com>
2026-09-03 20:46:54 +0530

    chnages

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.na3uq3k5je8"
+    "revision": "0.48tkqp05jp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
